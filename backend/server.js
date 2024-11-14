92217a76f98c9a10a83c9aa9649f8f45dfd6840d
tranchung163 <tranchung163@gmail.com>
2024-11-14 13:32:32 -0800

    set up env

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+
+const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 5001; // Change the port number here
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+  });
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
