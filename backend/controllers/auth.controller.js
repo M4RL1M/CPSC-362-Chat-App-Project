69c22ec44adeb3373a409fa2f7834df41d2da85b
tranchung163 <tranchung163@gmail.com>
2024-11-14 15:13:39 -0800

    Updated MongoDB and created models

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,11 +1,30 @@
+import User from "../models/user.model.js";
+
+export const signup = async (req, res) => {            
+    try {
+      const {fullname, username, password, confirmPassword, gender} = req.body;
+      if(password !== confirmPassword) {
+        return res.status(400).json({message: "Password do not match"});
+      }
+      const user = await User.findOne({username});
+      if (user) {
+        return res.status(400).json({message: "User already exists"});
+      }
+
+      // Hash Password Here
+
+     // https://avatar-placeholder.iran.liara.run/
+      const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+
+    }   catch (error) {
+      console.log("Error in signup  ", error);  
+    }
+}
+
 export const login = (req, res) => {
     console.log("Login User");  
   } 
 
 export const logout = (req, res) => {
     console.log("Logout User");  
-  } 
-
-export const signup = (req, res) => {            
-    console.log("Signup User");  
-}
+  }
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
 
 const app = express();
-
-dotenv.config();
 const PORT = process.env.PORT || 5001; // Change the port number here
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
-  });
+dotenv.config();
+
+app.use(express.json()); // To parse the incoming requests with JSON payloads (from req.body)
 
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.get("/", (req, res) => {
+//     res.send("Hello World");
+//   });
+
+
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server is running on port ${PORT}`);
+});
 
