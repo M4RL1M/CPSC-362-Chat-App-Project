cd546a74cea71a3f770e1c770a2adbe8a92ba19b
tranchung163 <tranchung163@gmail.com>
2024-11-26 20:16:52 -0800

    update message container

--- frontend/src/components/message/MessageContainer.jsx
+++ frontend/src/components/message/MessageContainer.jsx
@@ -32,11 +32,12 @@ const MessageContainer = () => {
 export default MessageContainer;
 
 const NoChatSelected = () => {
+    const { authUser } = useAuthContext();
     return (
         <div className='flex items-center justify-center w-full h-full'>
             <div className='px-4 text-center sm:text-lg md-text-xl 
             text-gray-200 font-semibold flex flex-col items-center gap-2'>
-                <p>Welcome to</p>
+                <p>Welcome  {authUser.fullname}</p>
                 <p className='text-3xl text-blue-500'> ChatApp </p>
                 <p>Select a chat to start messaging</p>
                 <TiMessages className='text-3xl md:text-6xl text-center' />
